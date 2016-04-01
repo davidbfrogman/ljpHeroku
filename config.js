dd8999da91c01dca0ce802e71cbff63770e39cb5
Denver Engagement Photographer <info@davebrownphotography.com>
2016-03-31 19:03:18 -0600

    adding a margin just in case.

--- config.js
+++ config.js
@@ -12,7 +12,6 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
-
   map: {
     "ansi-styles": "npm:ansi-styles@2.2.1",
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.0-beta.1.1.1",
@@ -1101,4 +1100,4 @@ System.config({
       "window-size": "npm:window-size@0.1.0"
     }
   }
-});
+})
